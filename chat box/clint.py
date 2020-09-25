c69f3fb10ef5e25c989c8f61056c333677345b42
codeshoper <70807529+codeshoper@users.noreply.github.com>
2020-09-25 09:51:27 +0530

    Update clint.py

--- chat box/clint.py
+++ chat box/clint.py
@@ -2,33 +2,74 @@ import socket
 from tkinter import*
 from threading import *
 
-name="kartike"
+name="thor"
 cs=socket.socket()
-cs.connect(("192.168.0.105",5555))
-cs.send(bytes(name,'utf-8'))
-root=Tk()
+try:
+    cs.connect(("192.168.0.105",5555))
+    cs.send(bytes(name,'utf-8'))
+    root=Tk()
+    root.title("chat box")
+    root.config(bg="grey10")
+    scrollbar = Scrollbar(root,bg="grey10",activebackground="grey10")
+    scrollbar.pack( side = RIGHT, fill = Y )
+    ml=Listbox(root, yscrollcommand = scrollbar.set,height=28,width=30,bg="grey10",fg="grey80",bd=0,highlightthickness=0 )
+    ml.place(x=0,y=0)
+    scrollbar.config( command = ml.yview ,bg="grey10",activebackground="grey10")
+  
+
+except:
+    ro=Tk()
+    Label(ro,text="The server is not active").pack()
+    Button(ro,text="OK",command=(lambda : ro.destroy())).pack()
+    ro.mainloop()
 while True:
     
     def rec():
-        while True:
-            ap=cs.recv(1024).decode()
-            Label(root,text=ap).pack()
-
-    def send():
+        try:
+            while True:
+                ap=cs.recv(1024).decode()
+                k=""
+            
+                if len(ap)>25:
+                    for i in range(0,len(ap),25):
+                        k+=ap[i:i+25]+"\n"
+                    ap=k[0:len(k)-1]
+                ml.insert(END, ap)
+                ml.yview_moveto('1.0')
+        except:
+            print("disconnected from the server")
+    def nw():
+        rt=Tk()
+        b3=Button(rt,text="change name")
+        rt.mainloop()
+    def sendt():
         eg=e.get()
         cs.send(bytes(eg,"utf-8"))
+        k=""
+        
+        if len(eg)>25:
+            for i in range(0,len(eg),25):
+                k+=eg[i:i+25]+"\n"
+            eg=k[0:len(k)-1]
         if eg!="":
-            Label(root,text=(f"you:{eg}")).pack()
+            ml.insert(END, f"you:{eg}")
+            ml.yview_moveto('1.0')
+            e.delete(0,1025)
     try:
 
-        b=Button(root,text="send",command=send)
+        b=Button(root,text="send",command=sendt,bd=0,bg="grey10",fg="grey80")
         b.pack(side="bottom")
-        e=Entry(root)
+        #b=Button(root,text="...",command=nw,bd=0,bg="grey10",fg="grey80")
+        #b.pack(side="bottom")
+        e=Entry(root,bg="grey15",bd=0,fg="grey80",width=30,justify="center",insertbackground="grey80")
         e.pack(side="bottom")
         t=Thread(target=rec)
         t.start()
-        root.geometry("200x500+100+700")
-        root.mainloop() 
+        root.bind("<Return>",(lambda event:sendt()))
+        root.geometry("200x500+100+100")
+        root.resizable(0,0)
+        root.mainloop()
+        
         cs.close()
     except:
         break  
