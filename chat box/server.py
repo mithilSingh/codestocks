55d54834cf2099ab5a271e867293fa6b9de6be4f
codeshoper <70807529+codeshoper@users.noreply.github.com>
2020-09-25 09:53:38 +0530

    Update server.py

--- chat box/server.py
+++ chat box/server.py
@@ -9,30 +9,46 @@ print("waiting for connections")
 l=[]
 def send_it(li,c,msg,name):
     p=li.pop(li.index(c))
-    sk=l[0]
-    sk.send(bytes((f"{name}:{msg}"),"utf-8"))
+    for sk in li:
+        sk.send(bytes((f"{name}:{msg}"),"utf-8"))
     li.append(p)
 def getit(c,arr,name):
-    while True:
-        kt=c.recv(1024).decode()
-        print(arr,kt)
-        #Label(r,text=c.recv(128).decode()).pack()
-        #print(c.recv(1024))
-        #c.send(bytes("hi","utf-8"))
-        send_it(l,c,kt,name)
+    try:
+        while True:
+            kt=c.recv(1024).decode()
+            if kt[0:12]=="change_name:":
+                v=kt[12:len(kt)]
+                send_it(l,c,f"changed name to\'{v}\'",name)
+                name=v
+                
+            else:
+                print(name,kt)
+          
+                send_it(l,c,kt,name)
+    except:
+        print(f"disconnected from {name}")
+        l.pop(l.index(c))
+        
+        if len(l)==0:
+            
+            s.close()
+           
+        
         
 while True:
-    rk=Tk()
-    print ("done")
-    c,addr=s.accept()
-    namec=c.recv(2048).decode()
-    l.append(c)
-    
-    print(c)
-    print("connected with",addr)
-    t=Thread(target=getit,args=(c,addr,namec))
-    t.start()
-    #r.mainloop()
+    try:
+        rk=Tk()
         
-c.close()
-
+        c,addr=s.accept()
+        namec=c.recv(2048).decode()
+        l.append(c)
+        
+        
+        print("connected with",namec)
+        t=Thread(target=getit,args=(c,addr,namec))
+        t.start()
+    except:
+        break
+    
+    
+    
