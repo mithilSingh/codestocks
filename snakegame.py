c5682ec0d52b176371d84b2a3dcd3da3fccfa1d2
Mithil <70807529+codeshoper@users.noreply.github.com>
2021-03-25 10:49:42 +0530

    Add files via upload

--- snakegame.py
+++ snakegame.py
@@ -2,50 +2,82 @@ import  pygame as pg
 from random import randint
 pg.init()
 unit=10
-dis=pg.display.set_mode((400,400))
+window_width=400
+window_height=400
+dis=pg.display.set_mode((window_width,window_height))
 run=True
 clk=pg.time.Clock()
-no=None
-l=[]
-blocks=1
-score=0
+no="down"
+s_c=False
+blocks=3
+score=2
+toto=False
 food=False
 x,y=0,blocks*unit
-pos=[blocks*unit,blocks*unit]
+fps=10
+level="easy"
+l=[]
 font = pg.font.SysFont("comicsansms", 20)
 ghy=True
 pg.draw.rect(dis ,(255,255,255),pg.Rect(blocks*unit,blocks*unit,unit,unit))
+sscore=2
+h=False
 def lost():
-    global ghy
+    global ghy,fps
     text = font.render("you lost", True, (0, 128, 0))
     pg.draw.rect(dis ,(255,0,0),food_box)
     dis.blit(text,
         (   200,200))
-    
+    fps=0
     ghy=False
 while run:
     for eve in pg.event.get():
         if eve.type ==pg.QUIT:
             run=False
-
+        
         if eve.type == pg.KEYDOWN:
+            if eve.key==pg.K_RETURN and ghy==False:
+                no="down"
+                s_c=False
+                blocks=3
+                score=0 
+                sscore=0 
+                toto=False
+                food=False
+                x,y=0,blocks*unit
+                fps=10
+                level="easy"
+                l=[]
+                ghy=True
             if eve.key==pg.K_LEFT and no!="right":
                 no="left"
-            if eve.key==pg.K_RIGHT and no!="left":
+            elif eve.key==pg.K_RIGHT and no!="left": 
                 no="right"
-            if eve.key==pg.K_DOWN and no!="up":
+            elif eve.key==pg.K_DOWN and no!="up":
                 no="down"
-            if eve.key==pg.K_UP and no!="down":
+            elif eve.key==pg.K_UP and no!="down":
                 no="up"
+    if x>window_width:
+        no="right"
+        x=0
+    if x<0:
+        no="left"
+        x=window_width
     
+    if y>window_height:
+        no="down"
+        y=0
+    if y<0:
+        no="up"
+        y=window_height
     if ghy:
         if no=="up" :
             y+=-unit
-        if no=="down":
+        elif no=="down":
             y+=unit
-        if no=="right":
+        elif no=="right":
             x+=unit
-        if no=="left":
+        elif no=="left":
             x-=unit
     kia=[]
     for i in l[::-1]:
@@ -55,48 +87,103 @@ while run:
     pg.display.flip()    
     pg.display.flip()
     l.append([x,y])
-    clk.tick(10)
+    clk.tick(fps)
     dis.fill((0,0,0))
-    if x>400:
-        
-        x=0
-    if x<0:
-        
-        x=400
-    if y>400:
-        
-        y=0
-    if y<0:
-       
-        y=400
+    
     if len(l)>blocks:
         l=l[-blocks:]
+    
     if food==False:
-        fpx=randint(0,39)*10
-        fpy=randint(0,39)*10
+        fpx=randint(0,window_height/unit-1)*unit
+        fpy=randint(0,window_height/unit-1)*unit
         food_box=pg.Rect(fpx,fpy,unit,unit)
         
         food=True
+    
     try:
         if pg.Rect.contains(food_box,kia[0]):
-            print("collided")
+            sscore+=1 
+            print(pg.Rect.contains(food_box,kia[0]))
             food=False
             blocks+=1
             score+=1
             
     except IndexError:
-        print()
+        pass
+    if ghy:
+        for i in kia:
+            pg.draw.rect(dis,(255,255,255),i)
+            print("t")
+        pg.display.flip()
     try:
         if pg.Rect.collidelist(kia[0],kia[1:])!=-1:
             lost()
-            print("collided with itself")
+            
                  
     except IndexError:
         pass
-    text = font.render("score:"+str(score), True, (0, 128, 0))
-    pg.draw.rect(dis ,(255,0,0),food_box)
-    dis.blit(text,
-        (320,0))
+    def falseit():
+        global h
+        h=False
+    text = font.render("score:"+str(int(score)), True, (0, 128, 0))
+    pg.draw.rect(dis ,(0,0,0),food_box)
+    dop=pg.draw.circle(dis,(255,255,0),(fpx+((unit**2)/2)**0.5-2,fpy+(((unit)**2)/2)**0.5-2),(unit)/2,width=0)
+    
+    if level=="easy"  and s_c!=score:
+        if sscore%3==0 and toto:#shows
+            timen=pg.time.get_ticks()/1000
+            fps+=0
+            s_c=score
+            sscore=0
+            fx=randint(0,window_width/unit)*unit
+            fy=randint(0,window_height/unit)*unit
+            food_box_mega=pg.Rect(fx,fy,unit*2,unit*2)
+            h=True
+    if sscore!=0:
+        toto=True
+               
+    try:
+        if pg.Rect.contains(food_box_mega,kia[0]):
+            
+            h=False 
+            blocks+=5
+            score+=2.5
+            print(f"score:{score}")
+                    
+            toto=False
+    
+    except :
+        pass
+        #print(pg.Rect.contains(kia[0],food_box_mega,))
+    try:
+        if h:
+            pg.draw.circle(dis,(225,0,0),(fx+unit,fy+unit),unit)
+        #print("llll",pg.Rect.contains(kia[0],food_box_mega,))
+        
+    except NameError:
+        pass
+    try:
+        if pg.time.get_ticks()/1000-timen>5 and h:
+            h=False
+            
+            food_box_mega=None
+    
+            
+            
+    except:
+        pass
+
     pg.display.flip()
+    dis.blit(text,
+        (300,0))
+    pg.display.flip()
+    
+    
+    
+    if h:
+        lati=100-(pg.time.get_ticks()/1000-timen)*20
+        print((pg.time.get_ticks()/1000-timen)*20)
+        pg.draw.rect(dis,(255,0,0),pg.Rect(280,30,lati,10))
+
+        
 #pg.quit()
-print(pos)
