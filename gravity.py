b403cc65a5d9b7d2ccd11a8a709b8f0cececfb93
Mithil <70807529+codeshoper@users.noreply.github.com>
2021-06-30 03:59:06 +0530

    Update gravity.py

--- gravity.py
+++ gravity.py
@@ -1,6 +1,6 @@
 from math import*
 import pygame as pg
-
+from random import randint
 clk=pg.time.Clock()
 
 fps=40
@@ -9,38 +9,40 @@ dis=pg.display.set_mode((w,h))
 
 run=True
 
-G=0.0001
+G=0.000001
 
 class Mass:
-    def __init__(self,mass,color,coordinates):
+    def __init__(self,mass,color,coordinates,v):
         self.mass=mass
         self.color=color
         self.x,self.y=coordinates
         self.x_component,self.y_component=0,0
-        self.x_vel,self.y_vel=0,0
+        self.x_vel,self.y_vel=0,v
         self.radius=(self.mass)**(1/3)
     def update(self):
-        self.x_acc=self.x_component/self.mass
-        self.y_acc=self.y_component/self.mass
+        self.x_acc=self.x_component
+        self.y_acc=self.y_component
         self.y_vel+=self.y_acc
         self.x_vel+=self.x_acc
         self.x+=self.x_vel
         self.y+=self.y_vel
-
+        print(self.mass)
         pg.draw.circle(dis,self.color,(self.x,self.y),(self.radius))
 
 
     def forces(self):
         try:
+            self.x_component, self.y_component = 0, 0
+            self.y_acc, self.x_acc = 0, 0
             for i in l:
                 if i!=self:
                     y=self.y-i.y
                     x=self.x-i.x
                     r= sqrt((y)**2+(x)**2)
                     theta=asin(y/r)
-                    F=(G*i.mass*self.mass)/r*r
-                    self.x_component,self.y_component=0,0
-                    self.y_acc,self.x_acc=0,0
+                    F=(G*i.mass)/r*r
+
+
                     if x>0:
 
                         self.x_component+=-F*cos(theta)
@@ -55,11 +57,8 @@ class Mass:
             self.x_component, self.y_component = 0, 0
 
 
-l=[
-Mass(30,(255,255,255),(0,0)),
-Mass(30,(255,255,255),(0,h-10)),
-Mass(30,(255,255,255),(w-10,0)),
-Mass(30,(255,255,255),(w-10,h-10))
+l=[    Mass(20000,(255,255,255),(400,400),0)
+
 ]
 
 while run:
@@ -70,7 +69,7 @@ while run:
 
             run=False
         if pg.mouse.get_pressed()[0]:
-            l.append(Mass(30,(255,255,255),pg.mouse.get_pos()))
+            l.append(Mass(30,(255,255,255),pg.mouse.get_pos(),2))
     clk.tick(fps)
     for i in l:
         i.forces()
