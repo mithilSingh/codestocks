6f86bdcb4bb5bcb95b5eac0362906b117fd52de2
Mithil <70807529+codeshoper@users.noreply.github.com>
2021-07-03 21:36:59 +0530

    Update gravity.py

--- gravity.py
+++ gravity.py
@@ -1,10 +1,17 @@
+from licensing.models import *
+from licensing.methods import Key, Helpers
+
+
+
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from math import *
 import pygame as pg
 from random import randint
 from tkinter import *
+from tkinter import ttk
 from threading import Thread
+static=1
 pg.init()
 clk = pg.time.Clock()
 
@@ -18,6 +25,7 @@ G = 6.67*10**-7
 continue_ = 0
 lines = 1
 trac_cou=1
+music_count=1
 font = pg.font.Font('freesansbold.ttf', 100)
 font2 = pg.font.Font('freesansbold.ttf', 20)
 text = font.render('GRAVITY', True, (0,0,255), (0,0,0))
@@ -93,18 +101,20 @@ class Mass:
 
 
             except:
-                self.x_component, self.y_component = 0, 0
+                self.x_component, self.y_component = randint(-1,1),randint(-1,1)
 
 
 def specifications():
-    global e1, e2, e3, continue_, e4,l,root,k,t2,trac_cou,G
+    global e1, e2, e3, continue_, e4,l,root,k,t2,trac_cou,G,run
     def tracker():
         global  trac_cou
         trac_cou+=1
     def add_():
         global continue_, lines
         continue_ += 1
-
+    def motion():
+        global static
+        static+=1
     def line():
         global lines
         lines += 1
@@ -121,8 +131,25 @@ def specifications():
 
         G=10**int(a)
         text_.config(text=f"10^{G}")
-    k=True
-    root = Tk()
+
+    k = True
+    root2 = Tk()
+    root2.config(bg="black")
+    s = ttk.Style()
+    tabControl = ttk.Notebook(root2)
+
+    root = ttk.Frame(tabControl)
+    tab2 = ttk.Frame(tabControl)
+    tab3 = ttk.Frame(tabControl)
+
+
+    tabControl.add(root, text='Home')
+    tabControl.add(tab2, text='Settings')
+    tabControl.add(tab3, text='Help')
+    tabControl.pack(expand=1, fill="both")
+
+    #---------------------home---------------------------#
+
     Label(root, text="mass").pack()
     e1 = Entry(root)
     e1.pack()
@@ -150,6 +177,8 @@ def specifications():
     Checkbutton(state_frame, text="pause", command=add_).grid(column=0,row=0)
     Checkbutton(state_frame, text="lines", command=line).grid(column=1,row=0)
     Checkbutton(state_frame, text="tracker", command=tracker).grid(column=2, row=0)
+
+    Checkbutton(state_frame, text="static", command=motion).grid(column=3, row=0)
     Canvas(root, height=1, width=200, bg="grey").pack()
     G_frame=Frame(root)
     G_frame.pack()
@@ -158,16 +187,38 @@ def specifications():
     text_=Label(root,text="10^-3")
     text_.pack()
     Canvas(root, height=1, width=200, bg="grey").pack()
-    Button(root,text="clear enviourment",command=clear,padx=30).pack()
+    Button(root,text="clear environment",command=clear,padx=30).pack()
+    # ---------------------home---------------------------#
+    # ---------------------Settings---------------------------#
+    def music():
+        global music_count
+        music_count+=1
+        if music_count%2==0:
+            pg.mixer.music.stop()
+        else:
+            pg.mixer.music.load("alone.mp3")
+            pg.mixer.music.play(-1)
+
+
+
+    b=Checkbutton(tab2, text="Music", command=music)
+    b.pack(anchor=NW)
+    b.select()
+    # ---------------------Settings---------------------------#
+    # ---------------------help---------------------------#
+    Label(tab3,text="Don't act like an idiot and figure everything out by yourself").pack()
+    # ---------------------help---------------------------#
     t2=Thread(target=kill_it)
     t2.start()
-    root.mainloop()
+    root2.mainloop()
     print("llll")
 
     print("yo")
+    run=False
 t = Thread(target=specifications)
 t.daemon=True
-
+pg.mixer.music.load("alone.mp3")
+pg.mixer.music.play(-1)
 l = [Mass(10000,(w_ // 2, (h_ // 2)-50),(0,0),(40),True),Mass(100,((w_ // 2)-200, (h_ // 2)-50),(-1,-1),(2))]
 not_start=True
 while run:
@@ -179,11 +230,11 @@ while run:
         if pg.mouse.get_pressed()[0]:
             if not_start:
                 if simulating_textRect.collidepoint(pg.mouse.get_pos()):
-                    l=[]
+                    l=[]#Mass(100,((i//10)*20+300,(i%10)*20+300),(0,0),) for i in range(0,200)]
                     dis.fill((0,0,0))
                     not_start=False
                     t.start()
-                    G = 6.67 * 10 ** -4
+                    G = 6.67 * 10 ** -5
             else:
                 try:
                     m = int(e1.get())
@@ -204,7 +255,7 @@ while run:
                     yvel = -float(e3.get())
                 except:
                     yvel = 0
-                l.append(Mass(m, pg.mouse.get_pos(), (xvel, yvel), radius=rm))
+                l.append(Mass(m, pg.mouse.get_pos(), (xvel, yvel), radius=rm,k=static%2==0))
 
             # l.append(Mass(30, (255, 255, 255), pg.mouse.get_pos(), (0,0)))
         if not not_start:
@@ -229,3 +280,4 @@ while run:
 
 pg.quit()
 k=False
+#! Copyright (c) 2021 Mithil Inc. All Rights Reserved. *#
