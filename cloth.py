d2165207a64f803c10eb65b3e40da2e9754b7996
Mithil <70807529+codeshoper@users.noreply.github.com>
2022-01-27 18:35:04 +0530

    Update cloth.py

--- cloth.py
+++ cloth.py
@@ -2,15 +2,15 @@ from math import dist
 
 import pygame as pg
 
-width,height=1400,900
+width,height=1400,700
 dis = pg.display.set_mode((width,height))
 run=True
 clk=pg.time.Clock()
-fps=100
+fps=200
 gravity=9.8/fps
-bounse_constant=0.9
-friction=0.999
-
+bounse_constant=0.1
+friction=1
+dig=dist((height,width),(0,0))
 class Dot:
     def __init__(self, x,y,oldx,oldy,static=False):
 
@@ -18,7 +18,7 @@ class Dot:
         self.y=y
         self.oldx=oldx
         self.oldy=oldy
-        self.radius=3
+        self.radius=1
         self.static=static
     def update(self):
         if  self.static==False:
@@ -28,23 +28,23 @@ class Dot:
             self.x+=xv*friction
             self.y+=yv*friction
             if self.x>width:
-                self.x=width
+                self.x=width-1
                 self.oldx=width+bounse_constant*xv
             if self.y>height:
-                self.y=height
+                self.y=height-1
                 self.oldy=height+bounse_constant*yv
             if self.x<0:
-                self.x=0
+                self.x=1
                 self.oldx=self.x+bounse_constant*xv
             if self.y<0:
-                self.y=0
+                self.y=1
                 self.oldy=self.y+bounse_constant*yv
             
-        elif self.static=="l":
+        elif self.static=="move":
             self.x,self.y=pg.mouse.get_pos()
         
     def draw(self):
-        pg.draw.circle(dis,(255,0,255),(self.x,self.y),self.radius)
+        pg.draw.circle(dis,(255,255,255),(self.x,self.y),self.radius)
 class Stick:
     def __init__(self,p1,p2) :
         self.p1=p1
@@ -71,55 +71,52 @@ class Stick:
             self.p1.y-=yoffset
 
     def draw(self):
-        pg.draw.line(dis,(255,255,255),(self.p1.x,self.p1.y),(self.p2.x,self.p2.y))
+        pg.draw.line(dis,(255*(dist((0,0),(self.p1.x,self.p1.y))/dig),255*(dist((0,height),(self.p1.x,self.p1.y))/dig),255*(dist((width,0),(self.p1.x,self.p1.y))/dig)),(self.p1.x,self.p1.y),(self.p2.x,self.p2.y))
 def break_it():
     for i in range(len(sticks)):
-        if dist((sticks[i].p1.x,sticks[i].p1.y),pg.mouse.get_pos())<7:# or (sticks[i].p2.x,sticks[i].p2.y)==pg.mouse.get_pos():
+        if dist((sticks[i].p1.x,sticks[i].p1.y),pg.mouse.get_pos())<15:# or (sticks[i].p2.x,sticks[i].p2.y)==pg.mouse.get_pos():
             sticks.pop(i)
             break
         
 
 # dots=[
-#     Dot(100,100,120,95),
-#     Dot(200,100,200,200),
-#     Dot(200,200,200,200),
-#     Dot(100,200,100,200),
-#     Dot(210,200,230,200),
-#     Dot(260,200,260,200),
-#     Dot(310,200,290,200),
-#     Dot(350,100,100,200,),
+#     Dot(100,200,100,100),
+#     Dot(200,200,100,100),
+    
+#     Dot(300,200,100,200,"l"),
 # ]
 # sticks=[
+    
 #     Stick(dots[0],dots[1]),
 #     Stick(dots[1],dots[2]),
-#     Stick(dots[2],dots[3]),
-#     Stick(dots[3],dots[0]),
-#     Stick(dots[0],dots[2]),
-#     Stick(dots[1],dots[3]),
-#     Stick(dots[1],dots[-4]),
-#     Stick(dots[-4],dots[-3]),
-#     Stick(dots[-3],dots[-2]),
-#     Stick(dots[-2],dots[-1]),
+
 # ]
-# 
-# 
+
+
 
 
 
 dots=[]
 sticks=[]
 n=25
+de=20
 for i in range(n):
     for j in range(n):
-        dots.append(Dot(j*20+300,i*20,j*20+300,i*20))
+        dots.append(Dot(j*de+300,i*de,j*de+300,i*de))
 for i in range(n-1):
     for j in range(n-1):
         sticks.append(Stick(dots[i+j*n],dots[i+1+j*n] ))
         sticks.append(Stick(dots[i+j*n],dots[i+(j+1)*n] ))
+       
+        if i==n-2:
+            sticks.append(Stick(dots[i+1+j*n],dots[i+1+(j+1)*n] ))
+        if j==n-2:
+            sticks.append(Stick(dots[i+(j+1)*n],dots[i+1+(j+1)*n] ))
+
 dots[0].static=True
 dots[n-1].static=True
-# dots[n*n-n].static=True
-# dots[n*n-2].static="l"
+dots[n*n-n].static=True
+dots[n*n-1].static="move"
 
         
 while run:
@@ -128,6 +125,8 @@ while run:
             run=False
         if pg.mouse.get_pressed()[0]:
             break_it()
+        if pg.mouse.get_pressed()[2]:
+            print("hi")
     
     for i in dots:
         i.update()
@@ -139,4 +138,6 @@ while run:
     clk.tick(fps)
     pg.display.flip()
     dis.fill((0,0,0))
+    
+
     
