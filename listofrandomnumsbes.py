8826026455d57df563f274405f844fdf874bf14d
Mithil Singh <70807529+mithilSingh@users.noreply.github.com>
2025-04-20 20:47:41 +0530

    managed all the files

--- listofrandomnumsbes.py
+++
@@ -1,6 +0,0 @@
-import random as r
-def hi(start,end ,length):
-  for i in range(length):
-    r.randint(start,end)
-   
-  
--- sorting.py
+++
@@ -1,7 +0,0 @@
-from random import *
-a=[]
-for i in range(100):
-    a.append(randint(0,1000))
-print(a)
-
-while 
